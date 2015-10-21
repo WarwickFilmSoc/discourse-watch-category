a60c8645cf18a6bd47d1ba3c15574d01512cfa39
Thomas Purchas <tpurchas@gmail.com>
2015-10-21 19:24:19 +0100

    Make notification level change generic
    Use a hash mapping between groups and categories, rather than copy/pasta code for each group and category pair.

--- plugin.rb
+++ plugin.rb
@@ -1,28 +1,26 @@
 # name: Watch Category
 # about: Watches a category for all the users in a particular group
 # version: 0.2
-# authors: Arpit Jalan
-# url: https://github.com/discourse/discourse-watch-category-mcneel
+# authors: Arpit Jalan, Thomas Purchas
+# url: https://github.com/thomaspurchas/discourse-watch-category
 
 module ::WatchCategory
   def self.watch_category!
-    mcneel_private_category = Category.find_by_slug("mcneel-private")
-    mcneel_group = Group.find_by_name("mcneel")
-
-    unless mcneel_private_category.nil? || mcneel_group.nil?
-      mcneel_group.users.each do |user|
-        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], mcneel_private_category.id) unless watched_categories.include?(mcneel_private_category.id)
+    groups_cats = {
+      "exec": "exec",
+      "exec": "shadow-exec",
+    }
+    
+    groups_cats.each do |group_name, cat_slug|
+      category = Category.find_by_slug(cat_slug)
+      group = Group.find_by_name(group_name)
+      
+      unless category.nil? || group.nil?
+        group.users.each do |user|
+          watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+          CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+        end
       end
-    end
-
-    reseller_category = Category.find_by_slug("resellers")
-    reseller_group = Group.find_by_name("resellers")
-    return if reseller_category.nil? || reseller_group.nil?
-
-    reseller_group.users.each do |user|
-      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], reseller_category.id) unless watched_categories.include?(reseller_category.id)
     end
   end
 end
