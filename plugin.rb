e0348dd26a2a47b18aee254e64549942a9781582
Thomas Purchas <tpurchas@gmail.com>
2015-10-21 22:11:33 +0100

    Remember how associative arrays work

--- plugin.rb
+++ plugin.rb
@@ -8,22 +8,23 @@ enabled_site_setting :watching_enabled
 module ::WatchCategory
   def self.watch_category!
     groups_cats = {
-      "exec" => "exec",
-      "exec" => "shadow-exec",
-      "duty_managers" => "duty-managers",
-      "q_proj" => "proj",
-      "q_proj" => "show-reports",
-      "t_proj" => "show-reports"
+      "exec" => ["exec", "shadow-exec"],
+      "duty_managers" => ["duty-managers"],
+      "q_proj" => ["proj", "show-reports"],
+      "t_proj" => ["show-reports"]
     }
-    
-    groups_cats.each do |group_name, cat_slug|
-      category = Category.find_by_slug(cat_slug)
-      group = Group.find_by_name(group_name)
-      
-      unless category.nil? || group.nil?
-        group.users.each do |user|
-          watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-          CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+
+    groups_cats.each do |group_name, cats|
+      cats.each do |cat_slug|
+
+        category = Category.find_by_slug(cat_slug)
+        group = Group.find_by_name(group_name)
+        
+        unless category.nil? || group.nil?
+          group.users.each do |user|
+            watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+            CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+          end
         end
       end
     end
